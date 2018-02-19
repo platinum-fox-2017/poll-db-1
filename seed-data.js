f3765db1b0be82541a948255b0666b6cf0c0030e
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-19 15:49:22 +0700

    Release 0,1,2 (revised)

--- seed-data.js
+++ seed-data.js
@@ -8,10 +8,56 @@ let db = new sqlite3.Database('./poll.db', (err) => {
     console.log('Connected to the database.');
   });
 
-function readData(callback, closeDb){
+function readDataVoters(callback){
     fs.readFile('./voters.csv', 'UTF-8', (err, data) => {
         if (err) throw err;
-        let dataToArr = data.split('\n');
+        let dataToArr = data.trim().split('\n');
+        let result = [];
+        for(let i=0; i<dataToArr.length; i++){
+            let row = dataToArr[i].split(',');
+            result.push(row);
+        }
+        callback(result);
+      });
+}
+
+function insertDataVoters(data){
+    db.serialize(function() {
+        var stmt = db.prepare("INSERT INTO Voters (firstName, lastName, gender, age) VALUES (?, ?, ?, ?)");
+        for (var i = 1; i < data.length; i++) {
+            stmt.run(data[i][0], data[i][1], data[i][2], data[i][3]);
+        }
+        stmt.finalize();
+    });
+}
+
+function readDataPoliticians(callback){
+    fs.readFile('./politicians.csv', 'UTF-8', (err, data) => {
+        if (err) throw err;
+        let dataToArr = data.trim().split('\n');
+        let result = [];
+        for(let i=0; i<dataToArr.length; i++){
+            let row = dataToArr[i].split(',');
+            result.push(row);
+        }
+        callback(result);
+      });
+}
+
+function insertDataPoliticians(data){
+    db.serialize(function() {
+        var stmt = db.prepare("INSERT INTO Politicians (name, party, location, gradeCurrent) VALUES (?, ?, ?, ?)");
+        for (var i = 1; i < data.length; i++) {
+            stmt.run(data[i][0], data[i][1], data[i][2], data[i][3]);
+        }
+        stmt.finalize();
+    });
+}
+
+function readDataVotes(callback, closeDb){
+    fs.readFile('./votes.csv', 'UTF-8', (err, data) => {
+        if (err) throw err;
+        let dataToArr = data.trim().split('\n');
         let result = [];
         for(let i=0; i<dataToArr.length; i++){
             let row = dataToArr[i].split(',');
@@ -21,22 +67,21 @@ function readData(callback, closeDb){
       });
 }
 
-function insertData(data, callback){
-    for(let i=1; i<data.length-1; i++){
-        let sql = `INSERT INTO Voters (firstName, lastName, gender, age) Values ('${data[i][0]}', '${data[i][1]}', '${data[i][2]}', ${data[i][3]})`;
-        db.all(sql, [], (err) => {
-            if (err) {
-              throw err;
-            }
-            console.log(`${data[i][0]} ${data[i][1]} inserted`);
-          });
-    }
+function insertDataVotes(data, callback){
+    db.serialize(function() {
+        var stmt = db.prepare("INSERT INTO Votes (idVoters, idPoliticians) VALUES (?, ?)");
+        for (var i = 1; i < data.length; i++) {
+            stmt.run(data[i][0], data[i][1]);
+        }
+        stmt.finalize();
+    });
     callback();
 }
 
 function closeDb(){
     db.close();
-    console.log('Database closed.')
 }
 
-readData(insertData, closeDb);
+readDataVoters(insertDataVoters);
+readDataPoliticians(insertDataPoliticians)
+readDataVotes(insertDataVotes, closeDb)
--- setup.js
+++ setup.js
@@ -1,28 +1,12 @@
-//your code here
-/*
-CREATE TABLE Voters (
-    idVoters integer PRIMARY KEY,
-    firstName varchar(48) NOT NULL,
-    lastName varchar(48) NOT NULL,
-    gender varchar(48) NOT NULL,
-    age INT NOT NULL
-);
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs')
 
-CREATE TABLE Politicians (
-    idPoliticians integer PRIMARY KEY,
-    name varchar(48) NOT NULL,
-    party varchar(48) NOT NULL,
-    location varchar(48) NOT NULL,
-    gradeCurrent INT NOT NULL
-);
+var db = new sqlite3.Database('./poll.db');
 
-CREATE TABLE VotersPoliticians (
-    idVotersPoliticians integer PRIMARY KEY,
-    idVoters integer NOT NULL,
-    idPoliticians integer NOT NULL,
-    FOREIGN KEY (idVoters) REFERENCES Voters (idVoters),
-    FOREIGN KEY (idPoliticians) REFERENCES Politicians (idPoliticians)
-);
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS Voters (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, firstName VARCHAR(48) NOT NULL, lastName VARCHAR(48) NOT NULL, gender VARCHAR(48) NOT NULL, age INTEGER NOT NULL)");
+    db.run("CREATE TABLE IF NOT EXISTS Politicians (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(48) NOT NULL, party VARCHAR(2) NOT NULL, location VARCHAR(2) NOT NULL, gradeCurrent REAL NOT NULL)");
+    db.run("CREATE TABLE IF NOT EXISTS Votes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, idVoters INTEGER NOT NULL, idPoliticians INTEGER NOT NULL, FOREIGN KEY (idVoters) REFERENCES Voters (idVoters), FOREIGN KEY (idPoliticians) REFERENCES Politicians (idPoliticians))");
+});
 
-
-*/
+db.close();
