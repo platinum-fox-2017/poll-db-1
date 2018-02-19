d5ca4cc45d6d0ef65e0cbfac0f489b11ae889c00
agungcaproex <agung.caproex@gmail.com>
2018-02-19 10:59:32 +0700

    release 0

--- setup.js
+++ setup.js
@@ -1,1 +1,25 @@
 //your code here
+/* CREATE TABLE
+CREATE TABLE voters (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+gender TEXT,
+age INTEGER);
+
+CREATE TABLE politicians (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+party TEXT,
+location TEXT,
+grade_current NUMERIC);
+
+CREATE TABLE voting (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+voterId INTEGER,
+politicianId INTEGER,
+FOREIGN KEY (voterId) REFERENCES voters(id),
+FOREIGN KEY (politicianId) REFERENCES politicians(id));
+
+
+*/
