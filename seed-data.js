2868b3ae63d35bd9a0c31efb211a2beb97dabf3f
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-19 12:54:33 +0700

    Fix  Bug on Seed Data

--- seed-data.js
+++ seed-data.js
@@ -24,7 +24,7 @@ db.serialize(function() {
   console.log("Berhasil Memasukkan Data Politicians");
 
  var stmt = db.prepare("INSERT INTO Voters (first_name, last_name, gender,age) VALUES (?,?,?,?)");
-  for(let i = 1; i < politicians.length; i++){
+  for(let i = 1; i < voters.length; i++){
     let voter = voters[i].split(',');
     let first_name = voter[0];
     let last_name = voter[1];
@@ -35,10 +35,10 @@ db.serialize(function() {
   stmt.finalize();
   console.log("Berhasil Memasukkan Data Voters");
  var stmt = db.prepare("INSERT INTO Votes (voterId, politicianId) VALUES (?,?)");
-  for(let i = 1; i < politicians.length; i++){
-    let voter = voters[i].split(',');
-    let voterId = voter[0];
-    let politicianId = voter[1];
+  for(let i = 1; i < votes.length; i++){
+    let vote = votes[i].split(',');
+    let voterId = vote[0];
+    let politicianId = vote[1];
     stmt.run([voterId,politicianId]);
   }
   stmt.finalize();
