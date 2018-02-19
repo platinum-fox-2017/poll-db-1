a7d9072be334f5668f2c89f1e60b2adf5be8bf4d
yosaid92 <yohanessahrul92@gmail.com>
2018-02-19 21:20:07 +0700

    pol-db1

--- setup.js
+++ setup.js
@@ -1,1 +1,12 @@
-//your code here
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('poll.db');
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS politicians (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, party TEXT, location TEXT, grade_current REAL)");
+
+  db.run("CREATE TABLE IF NOT EXISTS voters (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, gender TEXT, age TEXT)");
+
+  db.run("CREATE TABLE IF NOT EXISTS votes (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, votersId INTEGER, politiciansId INTEGER, FOREIGN KEY(votersId) REFERENCES voters(id), FOREIGN KEY(politiciansId) REFERENCES politicians(id))");
+});
+ 
+db.close();
