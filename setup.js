79d5a79426e27088c7f8fb24a5ceccbd3c8aa8ce
Herby Herado <herby,herado@gmail.com>
2018-02-19 15:35:48 +0700

    Release 2

--- setup.js
+++ setup.js
@@ -1,1 +1,55 @@
 //your code here
+const sqlite3 = require('sqlite3').verbose()
+let db = new sqlite3.Database('poll.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+      }
+      console.log('Connected to the in-memory SQlite database.');
+})
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS Voters (
+        voter_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+        first_name TEXT NOT NULL, 
+        last_name TEXT NOT NULL, 
+        gender VARCHAR(10) NOT NULL, 
+        age INTEGER NOT NULL
+        )`, [], (err) => {
+        if (err) throw err
+        else {
+            console.log('Table created successfully')
+        }
+    })
+    db.run(`CREATE TABLE IF NOT EXISTS Politicians (
+        politician_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+        politician_name TEXT NOT NULL, 
+        party VARCHAR(2) NOT NULL, 
+        location VARCHAR(3) NOT NULL, 
+        grade_current REAL NOT NULL
+        )`, [], (err) => {
+        if (err) throw err
+        else {
+            console.log('Table created successfully')
+        }
+    })
+    db.run(`CREATE TABLE IF NOT EXISTS Votes (
+        vote_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        voter_id INTEGER NOT NULL, 
+        politician_id INTEGER NOT NULL, 
+        FOREIGN KEY (voter_id) REFERENCES Voters(voter_id), 
+        FOREIGN KEY (politician_id) REFERENCES Politicians(politician_id)
+        )`, [], (err) => {
+        if (err) throw err
+        else {
+            console.log('Table created successfully')
+        }
+    })
+})
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
