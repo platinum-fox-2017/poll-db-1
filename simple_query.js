a5e695240977b3d778e6b7435b47564fb8084931
unknown <fatimah.azzhr@gmail.com>
2018-02-19 19:21:59 +0700

    salah push sebelumnya

--- simple_query.js
+++ simple_query.js
@@ -10,25 +10,30 @@ db.serialize(() => {
     console.log(rows)
   })
 
-  db.all(`SELECT COUNT(*) AS totalVote, Politicians.name FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId WHERE Politicians.name = 'Olympia Snowe'`, (err, rows) => {
+  db.all(`SELECT COUNT(*) AS totalVote, Politicians.name FROM Votes 
+         LEFT JOIN Politicians ON Politicians.id = Votes.politicianId WHERE Politicians.name = 'Olympia Snowe'`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
 
-  db.all(`SELECT Politicians.name, COUNT(*) AS totalVote FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId WHERE Politicians.name LIKE 'Adam%' GROUP BY Politicians.name`, (err, rows) => {
+  db.all(`SELECT Politicians.name, COUNT(*) AS totalVote FROM Votes 
+         LEFT JOIN Politicians ON Politicians.id = Votes.politicianId WHERE Politicians.name LIKE 'Adam%' GROUP BY Politicians.name`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
 
-  db.all(`SELECT COUNT(*) AS totalVote, Politicians.name, Politicians.party, Politicians.location FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId GROUP BY Politicians.name ORDER BY totalVote DESC LIMIT 3`, (err, rows) => {
+  db.all(`SELECT COUNT(*) AS totalVote, Politicians.name, Politicians.party, Politicians.location FROM Votes
+         LEFT JOIN Politicians ON Politicians.id = Votes.politicianId GROUP BY Politicians.name
+         ORDER BY totalVote DESC LIMIT 3`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
 
-  db.all(`SELECT Voters.first_name, Voters.last_name, Voters.gender, Voters.age FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId LEFT JOIN Voters ON Voters.id = Votes.voterId WHERE Politicians.name = 'Olympia Snowe'`, (err, rows) => {
+  db.all(`SELECT Voters.first_name, Voters.last_name, Voters.gender, Voters.age FROM Votes 
+         LEFT JOIN Politicians ON Politicians.id = Votes.politicianId LEFT JOIN Voters ON Voters.id = Votes.voterId 
+         WHERE Politicians.name = 'Olympia Snowe'`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
 });
-
 db.close();
