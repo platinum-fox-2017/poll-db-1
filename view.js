fe453a1780b2a20dad6f23059c33526ccd87b90a
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-19 19:35:03 +0700

    relase 3 done

--- view.js
+++ view.js
@@ -3,7 +3,8 @@ const db = new sqlite3.Database('pollDB');
 
 db.serialize(function(){
   // 1. nama politisi, partai, gradeCurrent di partai R dan gradeCurrent 9 s/d 11
-  // db.all("SELECT name,party,gradeCurrent FROM Politicians WHERE party = 'R' AND gradeCurrent BETWEEN 9 AND 11",function(err, rows){
+  // db.all(`SELECT name,party,gradeCurrent FROM Politicians
+  //   WHERE party = 'R' AND gradeCurrent BETWEEN 9 AND 11`,function(err, rows){
   //   if(err){
   //     console.log(err);
   //   } else {
@@ -12,7 +13,8 @@ db.serialize(function(){
   // });
 
   //2. jumlah vote untuk politisi Olympia Snowe
-  // db.get("SELECT count(*) AS totalVote, (SELECT name FROM Politicians WHERE id = politicianId) AS name FROM Votes WHERE politicianId = 17", function(err,rows){
+  // db.get(`SELECT count(*) AS totalVote, (SELECT name FROM Politicians
+  // WHERE id = politicianId) AS name FROM Votes WHERE politicianId = 17`, function(err,rows){
   //   if(err){
   //     console.log(err);
   //   } else {
@@ -21,25 +23,35 @@ db.serialize(function(){
   // });
 
   // 3. jumlah vote untuk politisi yang namanya ada kata 'Adam' --- belom
-  db.each("SELECT name, (SELECT count(*) FROM Votes WHERE name LIKE '%Adam%') AS totalVote FROM Politicians INNER JOIN Votes ON Votes.politicianId = Politicians.id WHERE name LIKE '%Adam%'", function(err,rows){
-    if(err){
-      console.log(err);
-    } else {
-      console.log(rows);
-    }
-  });
-
-  // 4. 3 nama, partai dan lokasi politisi yang memiliki suara terbanyak
-  // db
-
-  // 5. siapa yang milih olympia snowe
-  // db.all("", function(err, rows){
+  // db.all(`SELECT name, count(*) AS totalVote FROM Votes LEFT JOIN Politicians
+  // ON Votes.politicianId = Politicians.id WHERE name LIKE '%Adam%' GROUP BY name`, function(err,rows){
   //   if(err){
   //     console.log(err);
   //   } else {
   //     console.log(rows);
   //   }
   // });
+
+  // 4. 3 nama, partai dan lokasi politisi yang memiliki suara terbanyak
+  // db.all(`SELECT count(*) AS totalVote, name, party, locatian, gradeCurrent FROM Votes
+  // LEFT JOIN Politicians ON Votes.politicianId = Politicians.id GROUP BY name ORDER
+  // BY totalVote DESC LIMIT 3`, function(err,rows){
+  //   if(err){
+  //     console.log(err);
+  //   } else {
+  //     console.log(rows);
+  //   }
+  // });
+
+  // 5. siapa yang milih olympia snowe
+  db.all(`SELECT firstName, lastName, gender, age FROM Voters LEFT JOIN Votes ON Votes.voterId = Voters.id
+    WHERE politicianId = 17`, function(err, rows){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(rows);
+    }
+  });
 });
 
 db.close();
