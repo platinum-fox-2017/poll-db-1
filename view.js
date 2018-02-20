f4087fbbaa7a78e10a8985cf4f746aa4e226c3d5
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-20 07:56:32 +0700

    dirapihin

--- view.js
+++ view.js
@@ -3,8 +3,10 @@ const db = new sqlite3.Database('pollDB');
 
 db.serialize(function(){
   // 1. nama politisi, partai, gradeCurrent di partai R dan gradeCurrent 9 s/d 11
-  // db.all(`SELECT name,party,gradeCurrent FROM Politicians
-  //   WHERE party = 'R' AND gradeCurrent BETWEEN 9 AND 11`,function(err, rows){
+  // db.all(`SELECT name,party,gradeCurrent
+  //   FROM Politicians
+  //   WHERE party = 'R' AND gradeCurrent
+  //   BETWEEN 9 AND 11`,function(err, rows){
   //   if(err){
   //     console.log(err);
   //   } else {
@@ -13,8 +15,11 @@ db.serialize(function(){
   // });
 
   //2. jumlah vote untuk politisi Olympia Snowe
-  // db.get(`SELECT count(*) AS totalVote, (SELECT name FROM Politicians
-  // WHERE id = politicianId) AS name FROM Votes WHERE politicianId = 17`, function(err,rows){
+  // db.get(`SELECT COUNT(*) AS totalVote,
+  // (SELECT name FROM Politicians
+  //   WHERE id = politicianId) AS name
+  // FROM Votes
+  // WHERE politicianId = 17`, function(err,rows){
   //   if(err){
   //     console.log(err);
   //   } else {
@@ -22,9 +27,13 @@ db.serialize(function(){
   //   }
   // });
 
-  // 3. jumlah vote untuk politisi yang namanya ada kata 'Adam' --- belom
-  // db.all(`SELECT name, count(*) AS totalVote FROM Votes LEFT JOIN Politicians
-  // ON Votes.politicianId = Politicians.id WHERE name LIKE '%Adam%' GROUP BY name`, function(err,rows){
+  // 3. jumlah vote untuk politisi yang namanya ada kata 'Adam'
+  // db.all(`SELECT name, COUNT(*) AS totalVote
+  // FROM Votes
+  // JOIN Politicians
+  //   ON Votes.politicianId = Politicians.id
+  // WHERE name LIKE '%Adam%'
+  // GROUP BY name`, function(err,rows){
   //   if(err){
   //     console.log(err);
   //   } else {
@@ -33,9 +42,13 @@ db.serialize(function(){
   // });
 
   // 4. 3 nama, partai dan lokasi politisi yang memiliki suara terbanyak
-  // db.all(`SELECT count(*) AS totalVote, name, party, locatian, gradeCurrent FROM Votes
-  // LEFT JOIN Politicians ON Votes.politicianId = Politicians.id GROUP BY name ORDER
-  // BY totalVote DESC LIMIT 3`, function(err,rows){
+  // db.all(`SELECT count(*) AS totalVote, name, party, location, gradeCurrent
+  // FROM Votes
+  // JOIN Politicians
+  //   ON Votes.politicianId = Politicians.id
+  // GROUP BY name
+  // ORDER BY totalVote DESC
+  // LIMIT 3`, function(err,rows){
   //   if(err){
   //     console.log(err);
   //   } else {
@@ -44,7 +57,10 @@ db.serialize(function(){
   // });
 
   // 5. siapa yang milih olympia snowe
-  db.all(`SELECT firstName, lastName, gender, age FROM Voters LEFT JOIN Votes ON Votes.voterId = Voters.id
+  db.all(`SELECT firstName, lastName, gender, age
+    FROM Voters
+    JOIN Votes
+      ON Votes.voterId = Voters.id
     WHERE politicianId = 17`, function(err, rows){
     if(err){
       console.log(err);
